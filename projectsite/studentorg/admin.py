f9cb2e28ee28de836d07d255a33b430ce59a49d5
xxyy1313 <202380160@psu.palawan.edu.ph>
2026-02-16 14:08:46 +0800

    Edited admin refractor

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
-from .models import Student, OrgMember
+from .models import College, Program, Organization, Student, OrgMember
 
 
+# ===== OWNER TASKS =====
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "created_at", "updated_at")
+    search_fields = ("college_name",)
+    list_filter = ("created_at",)
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("prog_name", "college__college_name")
+    list_filter = ("college",)
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")
+    search_fields = ("name", "description")
+    list_filter = ("college",)
+
+
+# ===== COLLABORATOR TASKS =====
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname", "firstname", "middlename", "program")
