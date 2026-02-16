443b497255e47d0f957b04e1d61ed58f498c73ea
xxyy1313 <202380160@psu.palawan.edu.ph>
2026-02-16 14:32:42 +0800

    Edited Admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
+
 @admin.register(College)
 class CollegeAdmin(admin.ModelAdmin):
     list_display = ("college_name", "created_at", "updated_at")
     search_fields = ("college_name",)
     list_filter = ("created_at",)
+
 
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
@@ -22,7 +24,6 @@ class OrganizationAdmin(admin.ModelAdmin):
 
 
 @admin.register(Student)
-
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname", "firstname", "middlename", "program")
     search_fields = ("lastname", "firstname")
@@ -37,26 +38,3 @@ class OrgMemberAdmin(admin.ModelAdmin):
         return obj.student.program
 
     get_member_program.short_description = "Program"
-
-
-
-@admin.register(OrgMember)
-
-@admin.register(College)
-class CollegeAdmin(admin.ModelAdmin):
-    list_display = ("college_name", "created_at", "updated_at")
-    search_fields = ("college_name",)
-    list_filter = ("created_at",)
-
-
-@admin.register(Program)
-class ProgramAdmin(admin.ModelAdmin):
-    list_display = ("prog_name", "college")
-    search_fields = ("prog_name", "college__college_name")
-    list_filter = ("college",)
-
-@admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ("name", "college", "description")
-    search_fields = ("name", "description")
-    list_filter = ("college",)
