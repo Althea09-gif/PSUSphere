8318ea9ec581a0a44d4f4e8ca2be17dd1c254aea
xxyy1313 <202380160@psu.palawan.edu.ph>
2026-02-23 19:36:02 +0800

    CreateView, Program List Form Added in Program, Changed Typography in base.html to program

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -113,4 +113,11 @@ class ProgramListView(ListView):
    paginate_by = 5
 
 
+class ProgramCreateView(CreateView):
+  model = Program
+  form_class = ProgramForm
+  template_name = 'program_form.html'
+  success_url = reverse_lazy('program-list')
+
+
 
